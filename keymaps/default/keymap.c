4faf718baba71dc2a048fce916aefed2c5bc0eb2
argonarch <armando12123434@gmail.com>
2024-07-21 05:25:29 -0300

    add clang format add return false of sendtext() fix keys position change key grave position

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -54,35 +54,37 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
       send_string(my_str);
       SEND_STRING(SS_TAP(X_ENT));
-    } return true;
+    }
+    return false;
   }
   bool send_oracion(const char *my_str){
     if (record->event.pressed) {
       send_string(my_str);
-    } return true;
+    }
+    return false;
   }
   switch (keycode) {
-    case N_1: send_oracion_enter(V_N_1); break;
-    case N_2: send_oracion_enter(V_N_2); break;
-    case ID_1: send_oracion_enter(V_ID_1); break;
-    case P_1: send_oracion_enter(V_P_1); break;
-    case P_2: send_oracion_enter(V_P_2); break;
-    case P_3: send_oracion_enter(V_P_3); break;
-    case P_4: send_oracion_enter(V_P_4); break;
-    case P_5: send_oracion_enter(V_P_5); break;
-    case P_6: send_oracion_enter(V_P_6); break;
-    case P_7: send_oracion_enter(G_C1); break;
-    case P_8: send_oracion_enter(G_C2); break;
-    case Mail_1: send_oracion(V_Mail_1); break;
-    case Mail_2: send_oracion(V_Mail_2); break;
-    case Mail_3: send_oracion(V_Mail_3); break;
-    case Mail_4: send_oracion(V_Mail_4); break;
-    case Mail_C1: send_oracion(V_Mail_C1); break;
-    case Mail_C2: send_oracion(V_Mail_C2); break;
-    case Mail_C3: send_oracion(V_Mail_C3); break;
-    case IP_1: send_oracion(V_IP_1); break;
-    case IP_2: send_oracion(V_IP_2); break;
-    case IP_0: send_oracion(V_IP_0); break;
+    case N_1: return send_oracion_enter(V_N_1); break;
+    case N_2: return send_oracion_enter(V_N_2); break;
+    case ID_1: return send_oracion_enter(V_ID_1); break;
+    case P_1: return send_oracion_enter(V_P_1); break;
+    case P_2: return send_oracion_enter(V_P_2); break;
+    case P_3: return send_oracion_enter(V_P_3); break;
+    case P_4: return send_oracion_enter(V_P_4); break;
+    case P_5: return send_oracion_enter(V_P_5); break;
+    case P_6: return send_oracion_enter(V_P_6); break;
+    case P_7: return send_oracion_enter(G_C1); break;
+    case P_8: return send_oracion_enter(G_C2); break;
+    case Mail_1: return send_oracion(V_Mail_1); break;
+    case Mail_2: return send_oracion(V_Mail_2); break;
+    case Mail_3: return send_oracion(V_Mail_3); break;
+    case Mail_4: return send_oracion(V_Mail_4); break;
+    case Mail_C1: return send_oracion(V_Mail_C1); break;
+    case Mail_C2: return send_oracion(V_Mail_C2); break;
+    case Mail_C3: return send_oracion(V_Mail_C3); break;
+    case IP_1: return send_oracion(V_IP_1); break;
+    case IP_2: return send_oracion(V_IP_2); break;
+    case IP_0: return send_oracion(V_IP_0); break;
   } return true;
 }
 
@@ -92,36 +94,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               { TAB_NUM, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_H,    KC_L,    KC_LBRC, KC_RBRC, SYMBOL,  },
               { ESC_CON, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_R,    KC_T,    KC_N,    KC_S,    KC_MINS, XXXXXXX, FN_ENT,  },
               { S_DEL,   XXXXXXX, KC_COLN, KC_Q,    KC_X,    KC_K,    KC_J,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    XXXXXXX, KC_BSPC, },
-              { LC_F20,  OLCAG,   KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, W_SP,    XXXXXXX, XXXXXXX, XXXXXXX, C_CAPS,  OHYPR,   LSA_P,   QWERT,   }, },
+              { LC_F20,  OLCAG,   KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, W_SP,    XXXXXXX, XXXXXXX, XXXXXXX, C_CAPS,  LSA_P,   QWERT,   OHYPR,   }, },
               /*------------------------------------------------------------------------------------------------------------------------------*/
   [_QWERTY]={ { KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SLSH, KC_EQL,  _______, },
               { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, _______, },
               { KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT,  },
               { S_DEL,   XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, KC_BSPC, },
-              { KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, KC_LCTL, KC_RGUI, XXXXXXX, KC_RALT, QWERT,   }, },
+              { KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_RALT, QWERT,   KC_RGUI, }, },
               /*------------------------------------------------------------------------------------------------------------------------------*/
   [_NUM]  = { { KC_NO,   KC_1,    KC_2,    KC_3,    KC_PEQL, _______, _______, _______, _______, _______, _______, _______, _______, _______, },
               { _______, KC_4,    KC_5,    KC_6,    KC_0,    N_1,     P_1,     P_2,     P_3,     Mail_1,  Mail_C1, P_7,     Mail_4,  _______, },
               { KC_ESC,  KC_7,    KC_8,    KC_9,    KC_PDOT, N_2,     P_4,     P_5,     P_6,     Mail_2,  Mail_C2, P_8,     XXXXXXX, KC_ENT,  },
               { KC_LSFT, XXXXXXX, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, ID_1,    IP_1,    IP_2,    IP_0,    Mail_3,  Mail_C3, XXXXXXX, _______, },
-              { KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, KC_LCTL, KC_RGUI, XXXXXXX, KC_RALT, KC_NO,   }, },
+              { KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_RALT, KC_NO,   KC_RGUI, }, },
               /*------------------------------------------------------------------------------------------------------------------------------*/
   [_CON]  = { { KC_NO,   _______, _______, _______, _______, _______, RGB_RMOD,RGB_MOD, RGB_TOG, RGB_SPD, RGB_SPI, RGB_VAD, RGB_VAI, _______, },
               { KC_TAB,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_PEQL, _______, RESET,   KC_RSFT, },
               { _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_0, _______, XXXXXXX, KC_ENT,  },
               { KC_LSFT, XXXXXXX, _______, C_PRIOR, C_NEXT,  _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_PDOT, XXXXXXX, _______, },
-              { KC_LCTL, KC_LGUI, C_ST,    XXXXXXX, XXXXXXX, XXXXXXX, FN_ENT,  XXXXXXX, XXXXXXX, KC_LCTL, KC_RGUI, XXXXXXX, KC_RALT, KC_NO,   }, },
+              { KC_LCTL, KC_LGUI, C_ST,    XXXXXXX, XXXXXXX, XXXXXXX, FN_ENT,  XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_RALT, KC_NO,   KC_RGUI, }, },
               /*------------------------------------------------------------------------------------------------------------------------------*/
   [_FN]   = { { KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_AMPR, },
               { KC_BTN3, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, KC_WH_L, _______, _______, _______, KC_LNUM, KC_PSCR, KC_HOME, KC_END,  KC_PIPE, },
               { KC_ESC,  KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_WH_R, _______, _______, _______, KC_LCAP, KC_PGUP, KC_PGDN, XXXXXXX, _______, },
               { KC_LSFT, XXXXXXX, _______, C_PRIOR, C_NEXT,  _______, _______, _______, _______, _______, KC_INS,  KC_DEL,  XXXXXXX, _______, },
-              { KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, KC_LCTL, KC_RGUI, XXXXXXX, KC_RALT, KC_NO,   }, },
+              { KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_RALT, KC_NO,   KC_RGUI, }, },
               /*------------------------------------------------------------------------------------------------------------------------------*/
   [_SYMBOL]={ { CS_X,    CS_C,    CS_V,    CS_Z,    CS_Y,    CS_S,    _______, _______, _______, _______, KC_HASH, KC_QUES, KC_PLUS, KC_DLR,  },
-              { S_TAB,   KC_DQUO, KC_LABK, KC_RABK, KC_PERC, _______, _______, _______, _______, KC_GRV,  KC_UNDS, KC_LCBR, KC_RCBR, _______, },
-              { _______, KC_AT,   KC_CIRC, KC_EXLM, KC_ASTR, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, XXXXXXX, S_ENT,   },
+              { S_TAB,   KC_DQUO, KC_LABK, KC_RABK, KC_PERC, _______, _______, _______, _______, _______, KC_UNDS, KC_LCBR, KC_RCBR, _______, },
+              { _______, KC_AT,   KC_CIRC, KC_EXLM, KC_ASTR, KC_GRV,  _______, _______, _______, _______, KC_LPRN, KC_RPRN, XXXXXXX, S_ENT,   },
               { _______, XXXXXXX, _______, _______, _______, _______, KC_PWR,  KC_F15,  KC_SLEP, KC_WAKE, _______, _______, XXXXXXX, _______, },
-              { KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, KC_LCTL, KC_RGUI, XXXXXXX, KC_RALT, KC_NO,   }, },
+              { KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_RALT, KC_NO,   KC_RGUI, }, },
               /*------------------------------------------------------------------------------------------------------------------------------*/
 };
